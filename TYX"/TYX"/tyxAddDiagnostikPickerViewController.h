0382b82d879cab6d613f9b07a356b3eb97ee9625
Sandra Möller <sandra.moeller@techedgegroup.com>
2012-06-30 15:01:51 +0200

    addDiagnstic Logic
    Logic for adding diagnostics is implemented now

--- TYX"/TYX"/tyxAddDiagnostikPickerViewController.h
+++ TYX"/TYX"/tyxAddDiagnostikPickerViewController.h
@@ -1,21 +1,23 @@
 //
-//  tyxAddDiagnostikPickerViewController.h
+//  tyxAddDiagnostik.h
 //  Medical2Mobile
 //
-//  Created by Sandra Möller on 27.06.12.
+//  Created by Sandra Möller on 29.06.12.
 //  Copyright (c) 2012 Techedge. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "sqlite3.h"
 
-@interface tyxAddDiagnostikPickerViewController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate>
+@interface tyxAddDiagnostikPickerViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
 {
-   IBOutlet UIPickerView *activitytypePicker;
-    IBOutlet UILabel *selectedPickerRow;
-    IBOutlet UITextField *choosenDiagnosticNumber;
-    IBOutlet UITextField *shortDescription;
-    IBOutlet UITextView *longDescription;
+    sqlite3 *medicaldb;
+    NSString *databasePath;
 }
+@property (weak, nonatomic) IBOutlet UIPickerView *activitytypePicker;
 @property NSMutableArray *activitytype;
-- (IBAction)saveDiagnostic:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *selectedPickerRow;
+@property (weak, nonatomic) IBOutlet UITextField *choosenNumber;
+@property (weak, nonatomic) IBOutlet UITextField *shortDescription;
+@property (weak, nonatomic) IBOutlet UITextView *longDescription;
 @end
--- TYX"/TYX"/tyxAddDiagnostikPickerViewController.m
+++ TYX"/TYX"/tyxAddDiagnostikPickerViewController.m
@@ -8,8 +8,14 @@
 
 #import "tyxAddDiagnostikPickerViewController.h"
 
-@implementation tyxAddDiagnostikPickerViewController
+@implementation tyxAddDiagnostikPickerViewController 
+@synthesize activitytypePicker;
 @synthesize activitytype;
+@synthesize selectedPickerRow;
+@synthesize choosenNumber;
+@synthesize shortDescription;
+@synthesize longDescription;
+
 
 - (void)viewDidLoad
 {
@@ -22,20 +28,20 @@
 }
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
 
 - (void)viewDidUnload {
-    activitytypePicker = nil;
-    selectedPickerRow = nil;
-    choosenDiagnosticNumber = nil;
-    shortDescription = nil;
-    longDescription = nil;
+    [self setActivitytypePicker:nil];
+    [self setSelectedPickerRow:nil];
+    [self setChoosenNumber:nil];
+    [self setShortDescription:nil];
+    [self setLongDescription:nil];
     [super viewDidUnload];
 }
 
@@ -57,12 +63,40 @@
 
 #pragma mark PickerView Handling
 - (void)pickerView:(UIPickerView *)thePickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
-    
-    NSLog(@"Selected: %@. Index of selected: %i", [activitytype objectAtIndex:row], row);
+    //write selected pickerrow into a label for controll and further access
+    selectedPickerRow.text = [NSString stringWithFormat:@"%i", row];
 }
 #pragma mark saveData
 - (IBAction)saveDiagnostic:(id)sender 
 {
-    NSLog(@"Code for saving data goes here.");
+    const char *dbPath = [databasePath UTF8String];
+    //sqlite3_stmt *statement;
+    
+    if (sqlite3_open(dbPath, &medicaldb)==SQLITE_OK) {
+        //create SQL statement
+        NSString *querySQL = [NSString stringWithFormat:@"INSERT INTO tab_activities (fk_idpatient, fk_iduser, fk_idactivitytype, dateinsert, longdescription) VALUES ('1','1','\"%@\"','datetime()', '\"%@\"')",selectedPickerRow.text,longDescription.text];
+        NSLog(@"---------------------STATEMENT----------------");
+        NSLog(@"%@",querySQL);
+        NSLog(@"----------------------------------------------");
+        const char *query_stmt = [querySQL UTF8String];
+        char *error = NULL;
+        //send SQL statement to database
+        sqlite3_exec(medicaldb, query_stmt, NULL, NULL, &error);
+        if ( sqlite3_exec(medicaldb, query_stmt, NULL,NULL, &error) == SQLITE_OK)
+        {
+            NSLog(@"Inserted.");
+        }
+        else
+        {
+            NSLog(@"Error: %s", error);
+        }
+        
+//        sqlite3_prepare_v2(medicaldb, query_stmt, -1, &statement, NULL); 
+//            NSLog(@"hier bin ich");
+//            //fetch result of SQL statement
+//        sqlite3_step(statement);
+//        sqlite3_finalize(statement);
+        sqlite3_close(medicaldb);
+    }
 }
 @end
